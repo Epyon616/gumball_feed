8499fa86f582c5551e82158d043d36f8e7da37fb
StealthyGecko <lee@stealthygecko.com mailto:lee@stealthygecko.com>
2011-05-30 22:11:26 +0100

    added some basic keywords and a meta description

--- gumball_feed.rb
+++ gumball_feed.rb
@@ -4,11 +4,13 @@ require 'sinatra/static_assets'
 require 'twitter'
 
 before do
- @search = Twitter::Search.new
+  @title = "Gumball feed"
+  @keywords = "Gumball, 3000, rally, feed, tweets"
+  @description = "This application is a simple twitter based application which displays all tweets using the #Gumball3000 hashtag"
+  @search = Twitter::Search.new
 end
 
 get "/" do
-  @title = "Gumball feed"
   @hashtags = @search.hashtag("Gumball3000").language("en").no_retweets
   erb :index
 end
