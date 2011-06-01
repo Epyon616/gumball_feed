550c6b94b089201387f36194b1227fc4fabdb733
StealthyGecko <lee@stealthygecko.com mailto:lee@stealthygecko.com>
2011-06-01 08:48:41 +0100

    added helper to parse links in tweets and added image for footer link

--- gumball_feed.rb
+++ gumball_feed.rb
@@ -3,22 +3,20 @@ require 'sinatra'
 require 'sinatra/static_assets'
 require 'twitter'
 
+require File.expand_path(File.join(%w{ . lib/helpers.rb}))
+
 before do
   @title = "Gumball feed"
   @keywords = "Gumball, 3000, rally, feed, tweets"
   @description = "This application is a simple twitter based application which displays all tweets using the #Gumball3000 hashtag"
+  @author = "Designed & developed, all except for the Gumball logo by StealthyGecko 2011"
+
   @search = Twitter::Search.new
   @hashtags = @search.hashtag("Gumball3000").language("en").no_retweets
 end
 
-# helpers do
-#   def parse_text(text)
-#     tweet = text
-#   end
-# end 
 
 get "/" do
-  
   erb :index
 end
 
---
+++ lib/helpers.rb
@@ -0,0 +1,20 @@
+helpers do
+
+  def parse_text(text)
+    split_text = text.split
+    new_array = Array.new
+    split_text.each do |split|
+      if split.match(/http\:\/\/[a-zA-Z0-9*\D]{8,100}/)
+        new_array.push("<a href=\"#{split}\" target=\"_blank\">#{split}</a>")
+      elsif split.match(/@[a-zA-Z0-9*\D]{3,30}/)
+        new_split = split.gsub("@", "")
+        new_array.push("<a href=\"http://twitter.com/#{new_split}\" target=\"_blank\">#{split}</a>")
+      else
+        new_array.push(split)
+      end
+    end
+    parsed_text = new_array.join(" ")
+    return parsed_text
+  end
+
+end 
